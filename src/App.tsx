185b8f824b6c257274ab709e887fd06ad4c02154
Callum James Lodge <callumjlodge@gmail.com>
2025-11-03 16:44:08 +0000

    fixed issues with state not updating whole matrix when updated, added validation to prevent non-number inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   addMatrix,
   matrixMultiplication,
@@ -11,42 +11,31 @@ import {
 } from "../matrixChallenge.ts";
 import "./App.css";
 export default function App() {
-  let matARows = 4;
-  let matACols = 4;
-  let I: number[][] = [];
+  const matARows = 2;
+  const matACols = 2;
+  const I: string[][] = [];
   for (let i = 0; i < matARows; i++) {
     I.push([]);
   }
 
   for (let i = 0; i < matARows; i++) {
     for (let j = 0; j < matACols; j++) {
-      I[i][j] = 0;
+      I[i][j] = "";
     }
   }
 
   const [matrixA, setMatrixA] = useState(I);
-
-  let emptyA: number[][] = [];
-  for (let i = 0; i < matARows; i++) {
-    let row = [];
-    for (let j = 0; j < matACols; j++) {
-      row.push(0);
-    }
-    emptyA.push(row);
+  const inputA: number[][] = [];
+  for (let i = 0; i < matrixA.length; i++) {
+    inputA.push([]);
   }
-  console.log(emptyA);
-  const inputA = [
-    [1, 2, 1],
-    [2, 1, 1],
-    [2, 1, 2],
-  ];
-  const inputB = [
-    [2, 1, 2],
-    [1, 2, 1],
-    [2, 1, 2],
-  ];
-  const x = matrixMultiplication(inputA, inputB, 6);
-  //console.log(matrixA);
+  for (let i = 0; i < matrixA.length; i++) {
+    for (let j = 0; j < matrixA[0].length; j++) {
+      inputA[i][j] = Number(matrixA[i][j]);
+    }
+  }
+  console.log(inputA);
+
   function displayMatrix(x: number[][]) {
     return x.map((row, indexR) => (
       <section key={indexR} className={`row${indexR}`}>
@@ -61,26 +50,24 @@ export default function App() {
     ));
   }
   function handleEditMatrix(row: number, col: number, value: string) {
-    let tempMatrix: number[][] = matrixA;
-    let val: string = String(tempMatrix[row][col]) + value;
-    let valNum = Number(val);
-    tempMatrix[row][col] = valNum;
+    const tempMatrix: string[][] = [...matrixA];
+    tempMatrix[row][col] = value;
+    console.log(tempMatrix);
     setMatrixA(tempMatrix);
-    console.log(matrixA);
   }
   function handleShrinkRows() {
     if (matrixA.length > 2) {
-      let tempMatrix = [...matrixA];
+      const tempMatrix = [...matrixA];
       tempMatrix.splice(tempMatrix.length - 1, 1);
       setMatrixA(tempMatrix);
     }
   }
   function handleGrowRows() {
     if (matrixA.length < 6) {
-      let tempMatrix = [...matrixA];
+      const tempMatrix = [...matrixA];
       tempMatrix.push([]);
       for (let i = 0; i < matrixA[0].length; i++) {
-        tempMatrix[tempMatrix.length - 1].push(0);
+        tempMatrix[tempMatrix.length - 1].push("");
       }
       console.log(tempMatrix);
       setMatrixA(tempMatrix);
@@ -88,7 +75,7 @@ export default function App() {
   }
   function handleShrinkCols() {
     if (matrixA[0].length > 2) {
-      let tempMatrix = [...matrixA];
+      const tempMatrix = [...matrixA];
       for (let i = 0; i < matrixA.length; i++) {
         tempMatrix[i].splice(tempMatrix[i].length - 1, 1);
       }
@@ -97,10 +84,10 @@ export default function App() {
   }
   function handleGrowCols() {
     if (matrixA[0].length < 6) {
-      let tempMatrix = [...matrixA];
+      const tempMatrix = [...matrixA];
 
       for (let i = 0; i < matrixA.length; i++) {
-        tempMatrix[i].push(0);
+        tempMatrix[i].push("");
       }
       console.log(tempMatrix);
       setMatrixA(tempMatrix);
@@ -109,28 +96,36 @@ export default function App() {
   return (
     <>
       <h1>Hi</h1>
-      <main>
+      {/* <main>
         <div className="openBr"></div>
         <section>{displayMatrix(x)}</section>
         <div className="closeBr"></div>
-      </main>
+      </main> */}
       <section>
-        {matrixA.map((row, indexR) => (
-          <section key={indexR} className={`row${indexR}`}>
-            {row.map((_, indexC) => {
-              return (
-                <input
-                  onChange={(e) => {
-                    handleEditMatrix(indexR, indexC, e.target.value);
-                  }}
-                  key={indexC}
-                  className={`row${indexR} col${indexC}`}
-                  value={matrixA[indexR][indexC]}
-                />
-              );
-            })}
-          </section>
-        ))}
+        <section style={{ display: "inline-flex" }}>
+          <div className="openBr"></div>
+          <div>
+            {matrixA.map((row, indexR) => (
+              <form key={indexR} className={`row${indexR}`}>
+                {row.map((_, indexC) => {
+                  return (
+                    <input
+                      type="number"
+                      onChange={(e) => {
+                        handleEditMatrix(indexR, indexC, e.target.value);
+                      }}
+                      key={`col${indexC}row${indexR}`}
+                      className={`row${indexR} col${indexC} inputA`}
+                      value={matrixA[indexR][indexC]}
+                    />
+                  );
+                })}
+              </form>
+            ))}
+          </div>
+
+          <div className="closeBr"></div>
+        </section>
         <div className="buttonRows">
           <h2>Rows</h2>
           <button onClick={handleShrinkRows}>-</button>
