5e1ebedb9c40647b765f345c05a1143b3461f73b
Callum James Lodge <callumjlodge@gmail.com>
2025-11-03 14:54:15 +0000

    added buttons to add/remove lines from input matrix (up to 6x6 for now)

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,30 @@ import {
 } from "../matrixChallenge.ts";
 import "./App.css";
 export default function App() {
-  const [matrixA, setMatrixA] = useState([
-    [1, 0],
-    [0, 1],
-  ]);
-  const matARows = 2;
-  const matACols = 2;
-  let emptyA = [];
+  let matARows = 4;
+  let matACols = 4;
+  let I: number[][] = [];
+  for (let i = 0; i < matARows; i++) {
+    I.push([]);
+  }
+
+  for (let i = 0; i < matARows; i++) {
+    for (let j = 0; j < matACols; j++) {
+      I[i][j] = 0;
+    }
+  }
+
+  const [matrixA, setMatrixA] = useState(I);
+
+  let emptyA: number[][] = [];
   for (let i = 0; i < matARows; i++) {
     let row = [];
     for (let j = 0; j < matACols; j++) {
-      row.push([]);
+      row.push(0);
     }
     emptyA.push(row);
   }
+  console.log(emptyA);
   const inputA = [
     [1, 2, 1],
     [2, 1, 1],
@@ -52,10 +62,49 @@ export default function App() {
   }
   function handleEditMatrix(row: number, col: number, value: string) {
     let tempMatrix: number[][] = matrixA;
-    const valNum = Number(value);
+    let val: string = String(tempMatrix[row][col]) + value;
+    let valNum = Number(val);
     tempMatrix[row][col] = valNum;
     setMatrixA(tempMatrix);
     console.log(matrixA);
+  }
+  function handleShrinkRows() {
+    if (matrixA.length > 2) {
+      let tempMatrix = [...matrixA];
+      tempMatrix.splice(tempMatrix.length - 1, 1);
+      setMatrixA(tempMatrix);
+    }
+  }
+  function handleGrowRows() {
+    if (matrixA.length < 6) {
+      let tempMatrix = [...matrixA];
+      tempMatrix.push([]);
+      for (let i = 0; i < matrixA[0].length; i++) {
+        tempMatrix[tempMatrix.length - 1].push(0);
+      }
+      console.log(tempMatrix);
+      setMatrixA(tempMatrix);
+    }
+  }
+  function handleShrinkCols() {
+    if (matrixA[0].length > 2) {
+      let tempMatrix = [...matrixA];
+      for (let i = 0; i < matrixA.length; i++) {
+        tempMatrix[i].splice(tempMatrix[i].length - 1, 1);
+      }
+      setMatrixA(tempMatrix);
+    }
+  }
+  function handleGrowCols() {
+    if (matrixA[0].length < 6) {
+      let tempMatrix = [...matrixA];
+
+      for (let i = 0; i < matrixA.length; i++) {
+        tempMatrix[i].push(0);
+      }
+      console.log(tempMatrix);
+      setMatrixA(tempMatrix);
+    }
   }
   return (
     <>
@@ -66,7 +115,7 @@ export default function App() {
         <div className="closeBr"></div>
       </main>
       <section>
-        {emptyA.map((row, indexR) => (
+        {matrixA.map((row, indexR) => (
           <section key={indexR} className={`row${indexR}`}>
             {row.map((_, indexC) => {
               return (
@@ -76,11 +125,22 @@ export default function App() {
                   }}
                   key={indexC}
                   className={`row${indexR} col${indexC}`}
+                  value={matrixA[indexR][indexC]}
                 />
               );
             })}
           </section>
         ))}
+        <div className="buttonRows">
+          <h2>Rows</h2>
+          <button onClick={handleShrinkRows}>-</button>
+          <button onClick={handleGrowRows}>+</button>
+        </div>
+        <div className="buttonCols">
+          <h2>Columns</h2>
+          <button onClick={handleShrinkCols}>-</button>
+          <button onClick={handleGrowCols}>+</button>
+        </div>
       </section>
     </>
   );
